9dfa456f6a5c9be3d60f25d72a1479ff60924c48
luke munnich <lukemunnich@gmail.com>
2019-08-19 14:33:15 +0200

    added more php

--- index.php
+++ index.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Africa quizz</title>
 </head>
 <body>
-    <?php
+<?php
+error_reporting(0);
+
+
 //      CONFIGURATION
 $title = "Afica";
 $address = "index.html";
 $randomizequestions ="yes"; // set up as "no" to show questions without randomization
 //    END CONFIGURATION
+
 
 $a = array(
 1 => array(
@@ -56,129 +60,130 @@ $a = array(
    6 => 1
 ),
 6 => array(
-    0 => "what is the richest city in africa",
-    1 => "Cairo",
-    2 => "Cape Town",
-    3 => "Nairobe",
-    4 => "Pretoria",
-    6 => 2
- ),
- 7 => array(
-    0 => "what is South Africas main export",
-    1 => "minerals",
-    2 => "fruits and vegetables",
-    3 => "wine",
-    4 => "spices",
-    6 => 1
- ),
- 8 => array(
-    0 => "what is the most spoken launguage in africa",
-    1 => "Yoruba",
-    2 => "Swahili",
-    3 => "Shona",
-    4 => "Oromo",
-    6 => 2
- ),
- 9 => array(
-    0 => "what is Africas most played sport",
-    1 => "Soccer",
-    2 => "Rugby",
-    3 => "Boxing",
-    4 => "Golf",
-    6 => 1
- ),
- 10 => array(
-    0 => "which country is the most visited in Africa by tourists",
-    1 => "South Africa",
-    2 => "Morroco",
-    3 => "Zimbabwe",
-    4 => "Tanzania",
-    6 => 2
- ),
- 11 => array(
-    0 => "which country has the highest crime rate",
-    1 => "Libya",
-    2 => "Nigeria",
-    3 => "Congo",
-    4 => "South Africa",
-    6 => 4
- ),
- 12 => array(
-    0 => "which city was recently voted the best city in the world",
-    1 => "Nairoba",
-    2 => "Cape town",
-    3 => "Pretoria",
-    4 => "Lagos",
-    6 => 2
- ),
- 13 => array(
-    0 => "which country is rated the happiest by the world happiness report",
-    1 => "South Africa",
-    2 => "Nigeria",
-    3 => "Algeria",
-    4 => "Maritius",
-    6 => 4
- ),
- 14 => array(
-    0 => "Nelson Mandela was the president of which African country",
-    1 => "Tanzania",
-    2 => "Zimbabwe",
-    3 => "Congo",
-    4 => "South Africa",
-    6 => 4
- ),
- 15 => array(
-    0 => "what is the largest dessert in the world",
-    1 => "The Sahara",
-    2 => "Nyiri Desert",
-    3 => "Guban Desert",
-    4 => "Lompoul Desert",
-    6 => 1
- ),
- 16 => array(
-    0 => "which country is the biggest contributor to pollution",
-    1 => "South Africa",
-    2 => "Nigeria",
-    3 => "Somalia",
-    4 => "Eqypt",
-    6 => 2
- ),
- 17 => array(
-    0 => "which is the hottest country in Africa",
-    1 => "Congo",
-    2 => "Mexico",
-    3 => "Mali",
-    4 => "Libya",
-    6 => 4
- ),
- 18 => array(
-    0 => "which African countries have the most big 5 animals",
-    1 => "South Africa",
-    2 => "Namibia",
-    3 => "Botswana",
-    4 => "Congo",
-    6 => 3
- ),
- 19 => array(
-    0 => "which is the fastest growing country in Africa",
-    1 => "Egypt",
-    2 => "Nigeria",
-    3 => "South Africa",
-    4 => "Congo",
-    6 => 2
- ),
- 20 => array(
-    0 => "which country hosts the second biggest forest in the world",
-    1 => "Congo",
-    2 => "Egypt",
-    3 => "Kenya",
-    4 => "South Africa",
-    6 => 1
- ),
- );
- 
- $max=20;
- $question=$_POST["question"] ;
+   0 => "what is the richest city in africa",
+   1 => "Cairo",
+   2 => "Cape Town",
+   3 => "Nairobe",
+   4 => "Pretoria",
+   6 => 2
+),
+7 => array(
+   0 => "what is South Africas main export",
+   1 => "minerals",
+   2 => "fruits and vegetables",
+   3 => "wine",
+   4 => "spices",
+   6 => 1
+),
+8 => array(
+   0 => "what is the most spoken launguage in africa",
+   1 => "Yoruba",
+   2 => "Swahili",
+   3 => "Shona",
+   4 => "Oromo",
+   6 => 2
+),
+9 => array(
+   0 => "what is Africas most played sport",
+   1 => "Soccer",
+   2 => "Rugby",
+   3 => "Boxing",
+   4 => "Golf",
+   6 => 1
+),
+10 => array(
+   0 => "which country is the most visited in Africa by tourists",
+   1 => "South Africa",
+   2 => "Morroco",
+   3 => "Zimbabwe",
+   4 => "Tanzania",
+   6 => 2
+),
+11 => array(
+   0 => "which country has the highest crime rate",
+   1 => "Libya",
+   2 => "Nigeria",
+   3 => "Congo",
+   4 => "South Africa",
+   6 => 4
+),
+12 => array(
+   0 => "which city was recently voted the best city in the world",
+   1 => "Nairoba",
+   2 => "Cape town",
+   3 => "Pretoria",
+   4 => "Lagos",
+   6 => 2
+),
+13 => array(
+   0 => "which country is rated the happiest by the world happiness report",
+   1 => "South Africa",
+   2 => "Nigeria",
+   3 => "Algeria",
+   4 => "Maritius",
+   6 => 4
+),
+14 => array(
+   0 => "Nelson Mandela was the president of which African country",
+   1 => "Tanzania",
+   2 => "Zimbabwe",
+   3 => "Congo",
+   4 => "South Africa",
+   6 => 4
+),
+15 => array(
+   0 => "what is the largest dessert in the world",
+   1 => "The Sahara",
+   2 => "Nyiri Desert",
+   3 => "Guban Desert",
+   4 => "Lompoul Desert",
+   6 => 1
+),
+16 => array(
+   0 => "which country is the biggest contributor to pollution",
+   1 => "South Africa",
+   2 => "Nigeria",
+   3 => "Somalia",
+   4 => "Eqypt",
+   6 => 2
+),
+17 => array(
+   0 => "which is the hottest country in Africa",
+   1 => "Congo",
+   2 => "Mexico",
+   3 => "Mali",
+   4 => "Libya",
+   6 => 4
+),
+18 => array(
+   0 => "which African countries have the most big 5 animals",
+   1 => "South Africa",
+   2 => "Namibia",
+   3 => "Botswana",
+   4 => "Congo",
+   6 => 3
+),
+19 => array(
+   0 => "which is the fastest growing country in Africa",
+   1 => "Egypt",
+   2 => "Nigeria",
+   3 => "South Africa",
+   4 => "Congo",
+   6 => 2
+),
+20 => array(
+   0 => "which country hosts the second biggest forest in the world",
+   1 => "Congo",
+   2 => "Egypt",
+   3 => "Kenya",
+   4 => "South Africa",
+   6 => 1
+),
+);
+
+$max=20;
+
+$question=$_POST["question"] ;
 
 if ($_POST["Randon"]==0){
         if($randomizequestions =="yes"){$randval = mt_rand(1,$max);}else{$randval=1;}
@@ -223,7 +228,62 @@ function Goahead (number){
                 document.question.response.value="Incorrect"
         }
 }
-
+// -->
 </SCRIPT>
+
+</HEAD>
+<BODY BGCOLOR=FFFFFF>
+
+<CENTER>
+<H1><?php print "$title"; ?></H1>
+<TABLE BORDER=0 CELLSPACING=5 WIDTH=500>
+
+<?php if ($question<$max){ ?>
+
+<TR><TD ALIGN=RIGHT>
+<FORM METHOD=POST NAME="percentaje" ACTION="<?php print $URL; ?>">
+
+<BR>Percentaje of correct responses: <?php print $percentaje; ?> %
+<BR><input type=submit value="Next >>">
+<input type=hidden name=response value=0>
+<input type=hidden name=question value=<?php print $question; ?>>
+<input type=hidden name=ok value=<?php print $ok; ?>>
+<input type=hidden name=Randon value=<?php print $randval; ?>>
+<br><?php print $question+1; ?> / <?php print $max; ?>
+</FORM>
+<HR>
+</TD></TR>
+
+<TR><TD>
+<FORM METHOD=POST NAME="question" ACTION="">
+<?php print "<b>".$a[$randval2][0]."</b>"; ?>
+ 
+<BR>     <INPUT TYPE=radio NAME="option" VALUE="1"  onClick=" Goahead (1);"><?php print $a[$randval2][1] ; ?>
+<BR>     <INPUT TYPE=radio NAME="option" VALUE="2"  onClick=" Goahead (2);"><?php print $a[$randval2][2] ; ?>
+<?php if ($a[$randval2][3]!=""){ ?>
+<BR>     <INPUT TYPE=radio NAME="option" VALUE="3"  onClick=" Goahead (3);"><?php print $a[$randval2][3] ; } ?>
+<?php if ($a[$randval2][4]!=""){ ?>
+<BR>     <INPUT TYPE=radio NAME="option" VALUE="4"  onClick=" Goahead (4);"><?php print $a[$randval2][4] ; } ?>
+<?php if ($a[$randval2][5]!=""){ ?>
+<BR>     <INPUT TYPE=radio NAME="option" VALUE="5"  onClick=" Goahead (5);"><?php print $a[$randval2][5] ; } ?>
+<BR>     <input type=text name=response size=8>
+
+
+</FORM>
+
+<?php
+}else{
+?>
+<TR><TD ALIGN=Center>
+The Quiz has finished
+<BR>Percentaje of correct responses: <?php print $percentaje ; ?> %
+<p><A HREF="<?php print $address; ?>">Home Page</a>
+
+<?php } ?>
+
+</TD></TR>
+</TABLE>
+
+</CENTER>
 </body>
 </html>
