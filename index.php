652a06188f5a609dd6d7d52e1a241c104e381b79
luke munnich <lukemunnich@gmail.com>
2019-08-19 14:19:25 +0200

    added5 questions in arrays

--- index.php
+++ index.php
@@ -7,6 +7,53 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+//      CONFIGURATION
+$title = "Afica";
+$address = "index.html";
+$randomizequestions ="yes"; // set up as "no" to show questions without randomization
+//    END CONFIGURATION
+
+$a = array(
+1 => array(
+   0 => "what is Africas biggest economy",
+   1 => "Egypt",
+   2 => "South Africa",
+   3 => "Nigeria",
+   4 => "Cameroon",
+   6 => 3
+),
+2 => array(
+   0 => "in which country is the Nile river situated",
+   1 => "Congo",
+   2 => "Zimbabwe",
+   3 => "Mexico",
+   4 => "Egypt",
+   6 => 4
+),
+3 => array(
+   0 => "which is the biggest mountain in africa",
+   1 => "mount Kenya",
+   2 => "mount Karisimbi",
+   3 => "mount Stanley",
+   4 => "mount Kilimanjaro",
+   6 => 4
+),
+4 => array(
+   0 => "which country has the largest population",
+   1 => "Tanzania",
+   2 => "South Africa",
+   3 => "Nigeria",
+   4 => "Morroco",
+   6 => 3
+),
+5 => array(
+   0 => "which city has the largest population",
+   1 => "Lagos",
+   2 => "Cairo",
+   3 => "Johannesburg",
+   4 => "Abidyan",
+   6 => 1
+),
 </body>
 </html>
